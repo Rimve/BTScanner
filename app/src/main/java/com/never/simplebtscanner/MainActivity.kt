aeea8c3e1d465ceab5aae1b38b81b63e6774bdf8
Rimvydas <rimvydas02@gmail.com>
2023-05-06 16:46:10 +0300

    Add Hilt DI

--- app/src/main/java/com/never/simplebtscanner/MainActivity.kt
+++ app/src/main/java/com/never/simplebtscanner/MainActivity.kt
@@ -9,10 +9,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.never.simplebtscanner.ui.bt_scanner.ScannerScreen
 import com.never.simplebtscanner.ui.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber.DebugTree
 import timber.log.Timber.Forest
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
