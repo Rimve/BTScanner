7ddb1c6d18e08ae81bed20a44ccf980e08a7ac11
Rimvydas <rimvydas02@gmail.com>
2023-05-06 11:33:55 +0300

    Project setup

---
+++ app/src/main/java/com/never/simplebtscanner/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.never.simplebtscanner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.never.simplebtscanner.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
