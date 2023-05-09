6157da9d1fae0531abe566109c3c4fd9f69459de
Rimvydas <rimvydas02@gmail.com>
2023-05-09 18:38:27 +0300

    Add returns types in BTDeviceLocalRepository

--- app/src/main/java/com/never/simplebtscanner/ui/bt_scanner/utils/bt_device/database/BTDeviceLocalRepository.kt
+++ app/src/main/java/com/never/simplebtscanner/ui/bt_scanner/utils/bt_device/database/BTDeviceLocalRepository.kt
@@ -2,6 +2,7 @@ package com.never.simplebtscanner.ui.bt_scanner.utils.bt_device.database
 
 import com.never.simplebtscanner.data.AppDatabase
 import com.never.simplebtscanner.ui.bt_scanner.utils.bt_device.BTDeviceDomain
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -26,19 +27,23 @@ class BTDeviceLocalRepository @Inject constructor(private val database: AppDatab
             btDevice.copy(isSaved = false).toEntity()
         )
 
-    fun getBTDeviceList() = database.btDeviceDao().getAll().map { btDeviceEntityList ->
-        btDeviceEntityList
-            .map(BTDeviceEntity::toDomain)
-            .sortedBy { it.macAddress }
-    }
+    fun getBTDeviceList(): Flow<List<BTDeviceDomain>> = database.btDeviceDao()
+        .getAll()
+        .map { btDeviceEntityList ->
+            btDeviceEntityList
+                .map(BTDeviceEntity::toDomain)
+                .sortedBy { it.macAddress }
+        }
 
-    fun getSavedDeviceList() = database.btDeviceDao().getAllSaved().map { btDeviceEntityList ->
-        btDeviceEntityList
-            .map(BTDeviceEntity::toDomain)
-            .sortedBy { it.macAddress }
-    }
+    fun getSavedDeviceList(): Flow<List<BTDeviceDomain>> = database.btDeviceDao()
+        .getAllSaved()
+        .map { btDeviceEntityList ->
+            btDeviceEntityList
+                .map(BTDeviceEntity::toDomain)
+                .sortedBy { it.macAddress }
+        }
 
-    fun searchDevice(searchTerm: String) =
+    fun searchDevice(searchTerm: String): List<BTDeviceDomain> =
         database.btDeviceDao().findByNameOrMacAddress(searchTerm)
             .map(BTDeviceEntity::toDomain)
             .sortedBy { it.macAddress }
